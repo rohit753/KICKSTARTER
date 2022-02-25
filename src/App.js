5c0b4db80a2a22f10eab7a9fc1346d72787365dc
Ashwini Rathod <rathodashwini990@gmail.com>
2022-02-26 02:06:59 +0530

    completed form pages

--- src/App.js
+++ src/App.js
@@ -9,27 +9,22 @@
 // import Grid1 from './components/Grids/Grid1';
 // import Grid2 from './components/Grids/Grid2';
 
-import Section1 from "./components/StartProjectPage/Section1";
-import Section2 from "./components/StartProjectPage/Section2";
-import Section3 from "./components/StartProjectPage/Section3";
-import Section4 from "./components/StartProjectPage/Section4";
-import Section5 from "./components/StartProjectPage/Section5";
-import Section6 from "./components/StartProjectPage/Section6";
-import Section7 from "./components/StartProjectPage/Section7";
-import Section8 from "./components/StartProjectPage/Section8";
+// import Section1 from "./components/StartProjectPage/Section1";
+// import Section2 from "./components/StartProjectPage/Section2";
+// import Section3 from "./components/StartProjectPage/Section3";
+// import Section4 from "./components/StartProjectPage/Section4";
+// import Section5 from "./components/StartProjectPage/Section5";
+// import Section6 from "./components/StartProjectPage/Section6";
+// import Section7 from "./components/StartProjectPage/Section7";
+// import Section8 from "./components/StartProjectPage/Section8";
 
+// import Form1 from "./components/Forms/Form1"
+import Form2 from "./components/Forms/Form2"
 
 function App() {
   return (
     <div className="App">
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
-      <Section7 />
-      <Section8 />
+        <Form2 />
     </div>
   );
 }
